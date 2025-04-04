f5957d476eccfc5ea63a44430135438365f41e9f
Adrián Emir <135754894+aEmirsv@users.noreply.github.com>
2025-04-04 14:07:34 -0600

    SpringBoot Mandar correo

---
+++ mail/src/main/java/com/sending/mail/services/IEmailService.java
@@ -0,0 +1,9 @@
+package com.sending.mail.services;
+
+import com.sending.mail.services.models.EmailDTO;
+
+import jakarta.mail.MessagingException;
+
+public interface IEmailService {
+    public void sendMail(EmailDTO email)  throws MessagingException;
+}
